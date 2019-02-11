f3df1fde82a85d010b9ce39d084da537b6ba3f6c
Donatas Kimutis <DKIM@danskebank.lt>
2019-02-11 13:01:47 +0200

    connected H2 database and made Player class as entity

--- src/main/java/vu/lt/entities/Player.java
+++ src/main/java/vu/lt/entities/Player.java
@@ -1,9 +1,20 @@
 package vu.lt.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+@Table(name = "PLAYER")
 public class Player implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "NAME")
     private String name;
 
     public Player() {
@@ -19,5 +30,27 @@ public class Player implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(id, player.id) &&
+                Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
