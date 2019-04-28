0ad3bfa70aa31476aba4a92c1c9681bd0e67256f
kimutisd <mobilelifedl@MobileLfeDLsMBP.lan>
2019-04-28 14:29:33 +0300

    long calculation simulation when generating jersey number

---
+++ src/main/java/vu/lt/usecases/GeneratePlayerJerseyNumber.java
@@ -0,0 +1,43 @@
+package vu.lt.usecases;
+
+import vu.lt.interceptors.LoggedInvocation;
+import vu.lt.services.JerseyNumberGenerator;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@SessionScoped
+@Named
+public class GeneratePlayerJerseyNumber implements Serializable {
+    @Inject
+    JerseyNumberGenerator jerseyNumberGenerator;
+
+    private Future<Integer> jerseyNumberGenerationTask = null;
+
+    @LoggedInvocation
+    public String generateNewJerseyNumber() {
+        Map<String, String> requestParameters =
+                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        jerseyNumberGenerationTask = jerseyNumberGenerator.generateJerseyNumber();
+        return  "/playerDetails.xhtml?faces-redirect=true&playerId=" + requestParameters.get("playerId");
+    }
+
+    public boolean isJerseyGenerationRunning() {
+        return jerseyNumberGenerationTask != null && !jerseyNumberGenerationTask.isDone();
+    }
+
+    public String getJerseyGenerationStatus() throws ExecutionException, InterruptedException {
+        if (jerseyNumberGenerationTask == null) {
+            return null;
+        } else if (isJerseyGenerationRunning()) {
+            return "Jersey generation in progress";
+        }
+        return "Suggested jersey number: " + jerseyNumberGenerationTask.get();
+    }
+}
